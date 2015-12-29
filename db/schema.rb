23893c0b3b87e755138856fada98a63f09180d21
Daisuke Fujita <dtanshi45@gmail.com>
2015-12-29 16:19:37 +0900

    rails g model cart user:references item:references

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151229071828) do
+ActiveRecord::Schema.define(version: 20151229071915) do
+
+  create_table "carts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "carts", ["item_id"], name: "index_carts_on_item_id"
+  add_index "carts", ["user_id"], name: "index_carts_on_user_id"
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
