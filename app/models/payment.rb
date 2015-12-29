31b46567f666b7f75296f1817da809fa7aa42f46
Daisuke Fujita <dtanshi45@gmail.com>
2015-12-29 17:24:19 +0900

    Annotate models

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: payments
+#
+#  id         :integer          not null, primary key
+#  item_id    :integer
+#  price      :integer
+#  amount     :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Payment < ActiveRecord::Base
   belongs_to :item
 end
